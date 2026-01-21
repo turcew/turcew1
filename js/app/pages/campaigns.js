bde0138954d1603d5f42e7e7de01386dd0b09749
turcew <120652745+turcew@users.noreply.github.com>
2026-01-21 16:48:03 +0200

    Add campaigns component with data and lifecycle methods

---
+++ js/app/pages/campaigns.js
@@ -0,0 +1,21 @@
+export const campaigns = {
+    data:function(){
+        return {
+            parent:"",
+
+        }
+    },
+    mounted:function(){
+        this.parent = this.$parent.$parent;
+
+        if(!this.parent.user){
+            this.parent.logout();
+        }
+    },
+    methods:{
+
+    },
+    template:`
+    Campaigns
+    `
+};
