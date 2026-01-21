bae43f4702ad521385996647844260c2b9ff54e5
turcew <120652745+turcew@users.noreply.github.com>
2026-01-21 17:09:19 +0200

    Update popup.js

--- js/app/widgets/popup.js
+++ js/app/widgets/popup.js
@@ -1,1 +1,19 @@
-export const popup = {}
+export const popup = {
+    data:function(){
+        return {
+            parent:"",
+
+        }
+    },
+    mounted:function(){
+        this.parent = this.$parent.$parent;
+
+        }
+    },
+    methods:{
+
+    },
+    template:`
+    popup
+    `
+};
