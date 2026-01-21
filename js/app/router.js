a23f4274d8e4ef3e35f31f771757e64c754000cd
turcew <120652745+turcew@users.noreply.github.com>
2026-01-21 16:49:22 +0200

    Update router.js

--- js/app/router.js
+++ js/app/router.js
@@ -1,5 +1,5 @@
 import {login} from './pages/login.js';
-//import {campaigns} from './pages/campaigns.js';
+import {campaigns} from './pages/campaigns.js';
 //import {campaign} from './pages/campaign.js';
 //import {users} from './pages/users.js';
 //import {user} from './pages/user.js';
@@ -13,7 +13,7 @@ export const router = VueRouter.createRouter({
     history: VueRouter.createWebHashHistory(),
     routes: [
         {path: '/', name: 'Sign in', component: login},
-        //{path: '/campaigns', name: 'Campaigns', component: campaigns},
+        {path: '/campaigns', name: 'Campaigns', component: campaigns},
         //{path: '/campaign/:id', name: 'Campaign', component: campaign},
         //{path: '/users', name: 'Users', component: users},
         //{path: '/user/:id', name: 'User', component: user},
