229f45b81020938fb02beb7b2aa9ae34d148cb75
turcew <120652745+turcew@users.noreply.github.com>
2026-01-23 15:11:36 +0200

    Update app.js

--- js/app/app.js
+++ js/app/app.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const main = {
         data() {
             return {
-                url:"http://affiliate.yanbasok.com",
+                url:"https://affiliate.yanbasok.com",
                 user:{name:"", phone:"", email:"", date:"", auth:""},
                 FormData:{},
                 title:"",
@@ -110,6 +110,7 @@ document.addEventListener('DOMContentLoaded', function() {
     .use(router)
     .mount('#content')
 });
+
 
 
 
